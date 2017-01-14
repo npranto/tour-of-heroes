657e5043c2533b0bb1bd3a6dcbcebfca505d2bf1
Shaheen Ahmed <s.ahmed@aol.com>
2017-01-13 21:42:34 -0500

    adds routes to app components

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,9 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'
+import {RouterModule} from '@angular/router';
 
+import {DashboardComponent} from './dashboard.component';
 import {HeroesComponent}  from './heroes.component';
 import {HeroDetailComponent} from './hero-detail.component';
 import {AppComponent} from './app.component'
@@ -11,10 +13,21 @@ import {HeroService} from './hero.service';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: `heroes`,
+        component: HeroesComponent
+      },
+      {
+        path: `dashboard`,
+        component: DashboardComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroesComponent,
     HeroDetailComponent
   ],
